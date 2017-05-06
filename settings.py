8a229c777bd7e0544c2a3d4601311cacddd36fcb
Ben Sully <ben.sully@yougov.com>
2017-05-06 15:38:17 +0100

    Fix missing setting loading

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -9,3 +10,9 @@ API_SECRET = ''
 # Load environment variables from the .env file
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+# Load any extra environment variables overriding these settings
+for var, value in os.environ.items():
+    if var in globals():
+        globals()[var] = value
+
